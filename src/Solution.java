c5825b8a89dbd6f332ad355e484901a4273a1120
Adesh <adeshslad@gmail.com>
2026-05-02 13:02:34 +0530

    Added #268, Updated .gitignore, removed workspace.xml tracking

--- src/Solution.java
+++ src/Solution.java
@@ -151,4 +151,15 @@ public class Solution {
         return maxArea;
     }
 
+    // 268
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return expectedSum - sum;
+    }
+
 }
